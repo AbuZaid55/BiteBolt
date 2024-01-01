a44c148cef7651f50b6939b23a39601d7790c164
Abu Zaid <zaid70979@gmail.com>
2024-01-02 02:29:35 +0530

    frontend: Card1

--- frontend/src/app/components/Card1.tsx
+++ frontend/src/app/components/Card1.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
-import { FaRegStar } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
 import { FaIndianRupeeSign } from "react-icons/fa6";
 import { MdShoppingCart } from "react-icons/md";
+import { FaRegHeart } from "react-icons/fa";
+import { MdOutlineRemoveRedEye } from "react-icons/md";
 
 const Card1 = () => {
   return (
-    <div className=" bg-white shadow-lg rounded-lg p-4">
-    <img src="./img/3.png" alt="Image" />
-    <h1 className="text-2xl font-bold text-slate-700 my-3 text-center">Tasty Food</h1>
-    <div className="flex items-center justify-center"><FaRegStar/><FaRegStar/><FaRegStar/><FaRegStar/><FaRegStar/></div>
-    <div className="flex flex-col sm:flex-row items-center justify-between mt-5">
+    <div className=" bg-white shadow-lg relative rounded-lg p-4">
+    <div className='absolute top-0 left-0 p-4 text-xl w-full flex items-center justify-between'><span className=' cursor-pointer bg-slate-200 rounded-full p-2 hover:scale-125 hover:bg-main-800 hover:text-white text-slate-700 transition-all duration-200 ease-in-out'><FaRegHeart/></span><span className=' cursor-pointer bg-slate-200 rounded-full p-2 hover:scale-125 hover:bg-main-800 hover:text-white text-slate-700 transition-all duration-200 ease-in-out'><MdOutlineRemoveRedEye/></span></div>
+    <img className='w-[80%] mx-auto' src="./img/3.png" alt="Image" />
+    <h1 className="text-2xl font-bold text-slate-700 mt-3 mb-1 text-center">Tasty Food</h1>
+    <div className="flex items-center justify-center text-xl"><FaStar className=" text-main-800"/><FaStar className=" text-main-800"/><FaStar className=" text-main-800"/><FaStar className=" text-main-800"/><FaStar className=" text-slate-700"/></div>
+    <div className="flex flex-col sm:flex-row items-center justify-between mt-2">
       <h1 className="flex items-center justify-center text-2xl text-main-800"><FaIndianRupeeSign/><span className=" font-bold">15.99</span></h1>
-      <button className=" bg-slate-700 text-white px-8 py-2 rounded-md flex items-center justify-center w-full sm:w-auto mt-2 border-2 border-slate-700  hover:text-slate-700 hover:bg-[#2f2f2f1c] transition-all duration-300 ease-in-out"><span className='mr-2'>ADD</span><MdShoppingCart/></button>
+      <button className=" bg-slate-700 text-white px-8 py-2 rounded-md flex items-center justify-center w-full sm:w-auto mt-2 border-2 border-slate-700  hover:text-slate-700 hover:bg-[#3341551f] transition-all duration-300 ease-in-out"><span className='mr-2'>ADD</span><MdShoppingCart/></button>
     </div>
   </div>
   )
