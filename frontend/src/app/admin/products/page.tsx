8cd6af4310369335d8dd59b7f2ad3a266db36982
Abu Zaid <zaid70979@gmail.com>
2024-01-05 21:15:51 +0530

    frontend: manage products

---
+++ frontend/src/app/admin/products/page.tsx
@@ -0,0 +1,83 @@
+"use client"
+import React, { useState } from 'react'
+import { FaTrash, FaEdit, FaStar } from "react-icons/fa";
+import Link from 'next/link';
+import '../../CSS/table.css'
+import AdminSiderbar from '@/app/components/AdminSiderbar';
+import { Roboto_Slab } from "next/font/google"
+
+const robotoSlab = Roboto_Slab({
+  weight: "500",
+  subsets: ["greek"],
+  display: "swap",
+})
+
+const page = () => {
+  const [showConfirm, setShowConfirm] = useState(false)
+  const rat = 5
+  return (
+    <div className='flex bg-slate-200'>
+      <AdminSiderbar />
+      <div className='pl-20 md:pl-0 products w-full h-[100vh] overflow-hidden overflow-y-scroll text-slate-700' id='main'>
+        <h1 className={`${robotoSlab.className} text-4xl sm:text-5xl text-center my-6 text-main-800`}>Products</h1>
+            <div className='w-full flex flex-col items-start p-4'>
+              <input className=" outline-none w-full py-1 px-4 text-xl border-2 border-main-800 rounded-md rounded-bl-none shadow-md max-w-[600px]" type="search"  placeholder='Search Products' />
+              <label className='bg-main-800 text-white py-1 px-2 sm:px-4 rounded-b-md sm:text-xl shadow-md'>Search Type:- 
+                <select className='text-slate-700 sm:px-4 ml-2 outline-none rounded-md cursor-pointer'>
+                  <option value={"default"}>Default</option>
+                  <option value={"stock"}>Stock</option>
+                  <option value={"price"}>Price</option>
+                  <option value={"rating"}>Rating</option>
+                  <option value={"id"}>Product Id</option>
+                </select>
+              </label>
+            </div>
+            <div className='flex items-center justify-end w-full px-6'><span className='bg-main-800 text-white text-xl py-1 px-4 rounded-md shadow-md'>Total Products: 20</span></div>
+
+
+        <table className='mt-4'>
+          <thead className='bg-main-800 border-main-800 border-2'>
+            <tr>
+              <th>Thumbnail</th>
+              <th className='name'>Name</th>
+              <th>Stock</th>
+              <th>Price</th>
+              <th>Rating</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td className='border border-main-800' aria-label='Thumbnail'><img style={{ height: "60px", width: "60px", margin: " 8px auto", borderRadius: '5px' }} src={'/img/5.jpg'} alt="Pic" /></td>
+              <td className='name border border-main-800' aria-label={"Name"}>
+                <Link href={`/details`}><p className='w-full max-h-28 overflow-hidden'>Testy Food Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eligendi nisi earum?</p></Link>
+              </td>
+              <td className='border border-main-800' aria-label={"Stock"}>20</td>
+              <td className='border border-main-800' aria-label={"Price"}>&#8377; 2000</td>
+              <td className='border border-main-800' aria-label={"Rating"}>
+                <span className={`${(rat > 3) ? 'bg-green-700' : ""} ${(rat <= 3 && rat > 1) ? 'bg-yellow-500' : ""} ${(rat <= 1) ? 'bg-red-700' : ""} text-white flex m-auto items-center justify-evenly`} style={{ width: '50px', height: "30px", borderRadius: '8px' }}>{rat}<FaStar /></span>
+              </td>
+              <td className='border border-main-800' aria-label={"Edit"}><Link href={`/admin/dashboard/updateproduct`}><FaEdit className='icon edit' /></Link></td>
+              <td className='border border-main-800' aria-label={"Delete"}><FaTrash className='icon delete' onClick={() => { setShowConfirm(true) }} /></td>
+            </tr>
+            
+          </tbody>
+
+        </table>
+      </div>
+      <div className={`${(showConfirm) ? 'flex' : 'hidden'} fixed top-0 left-0 w-full h-full items-center justify-center bg-[#3341557f]`}>
+        <div className=' w-56 h-56 border p-4  bg-white rounded  flex items-stretch justify-between flex-col border-main-800 shadow-lg'>
+          <h1 className=' text-center text-2xl'>Are your sure you want to delete?</h1>
+          <div className='flex items-center justify-between'>
+            <button className=' bg-red-700 text-white px-3 py-2 rounded hover:bg-[#3341551f] border-2 border-red-700 hover:text-red-700 transition-all duration-300 ease-in-out'>YES</button>
+            <button className=' bg-main-800 text-white px-3 py-2 rounded hover:bg-[#44b67721] border-2 border-main-800 hover:text-main-800 transition-all duration-300 ease-in-out'>NO</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default page
--- frontend/src/app/components/AdminSiderbar.tsx
+++ frontend/src/app/components/AdminSiderbar.tsx
@@ -14,21 +14,21 @@ const robotoSlab = Roboto_Slab({
   
 
 const AdminSiderbar = () => {
-    const [open,setOpen]=useState(true)
+    const [open,setOpen]=useState(false)
   return (
-    <div className={`bg-white ${(open)?'w-72':"w-20"} h-full p-4 relative transition-all duration-300 ease-in-out`}>
-        <span onClick={()=>{setOpen(!open)}} className='absolute top-4 p-2 rounded-full text-2xl right-0 translate-x-1/2 cursor-pointer hover:scale-110 hover:text-white hover:bg-main-800 transition-all ease-in-out duration-300 shadow-md bg-slate-100'><FaAngleLeft/></span>
-        <Link href="/" className="flex items-center overflow-hidden mt-10 gap-4">
-          <img className=" min-w-12 h-12" src="/logo.png" alt="logo" />
-          <h1 className={`text-4xl text-slate-700 ${robotoSlab.className}`}>BiteBolt</h1>
+    <div className={`bg-white ${(open)?'w-72':"w-16 sm:w-20"} h-[100vh] p-2 sm:p-4 absolute z-30 md:relative transition-all duration-300 ease-in-out`}>
+        <span onClick={()=>{setOpen(!open)}} className={`absolute top-4 p-1 sm:p-2 rounded-full text-2xl right-0 translate-x-1/2 cursor-pointer hover:scale-110 hover:text-white hover:bg-main-800 transition-all ease-in-out duration-300 shadow-md bg-slate-100 ${(open)?"":" rotate-180"}`}><FaAngleLeft/></span>
+        <Link href="/" className="flex items-center overflow-hidden pl-2 sm:pl-0 mt-10 gap-4">
+          <img className="min-w-8 h-8 sm:min-w-12 sm:h-12" src="/logo.png" alt="logo" />
+          <h1 className={`text-2xl sm:text-4xl text-slate-700 ${robotoSlab.className}`}>BiteBolt</h1>
         </Link>
-        <Link href="/dashboard" className='flex items-center py-2 bg-slate-100 rounded-md px-2 gap-3 text-2xl mt-8 hover:bg-[#e2e8f0] transition-all duration-200 ease-in-out overflow-hidden text-nowrap'><MdSpaceDashboard className="min-w-8 "/>Dashboard</Link>
-        <Link href="/dashboard" className='flex items-center py-2 bg-slate-100 rounded-md px-2 gap-3 text-2xl mt-1 hover:bg-[#e2e8f0] transition-all duration-200 ease-in-out overflow-hidden text-nowrap '><FaUserFriends className="min-w-8 "/>Users</Link>
-        <Link href="/dashboard" className='flex items-center py-2 bg-slate-100 rounded-md px-2 gap-3 text-2xl mt-8 hover:bg-[#e2e8f0] transition-all duration-200 ease-in-out overflow-hidden text-nowrap '><FaShoppingCart className="min-w-8 "/>Products</Link>
-        <Link href="/dashboard" className='flex items-center py-2 bg-slate-100 rounded-md px-2 gap-3 text-2xl mt-1 hover:bg-[#e2e8f0] transition-all duration-200 ease-in-out overflow-hidden text-nowrap '><FaPlusSquare className="min-w-8 "/>Add Products</Link>
-        <Link href="/dashboard" className='flex items-center py-2 bg-slate-100 rounded-md px-2 gap-3 text-2xl mt-8 hover:bg-[#e2e8f0] transition-all duration-200 ease-in-out overflow-hidden text-nowrap '><FaNotesMedical className="min-w-8 "/>Orders</Link>
-        <Link href="/dashboard" className='flex items-center py-2 bg-slate-100 rounded-md px-2 gap-3 text-2xl mt-1 hover:bg-[#e2e8f0] transition-all duration-200 ease-in-out overflow-hidden text-nowrap '><FaTimes className="min-w-8 "/>Cancelled Orders</Link>
-        <Link href="/dashboard" className='flex items-center py-2 bg-slate-100 rounded-md px-2 gap-3 text-2xl mt-8 hover:bg-[#e2e8f0] transition-all duration-200 ease-in-out overflow-hidden text-nowrap '><FaCcAmazonPay className="min-w-8 "/>Payments</Link>
+        <Link href="/admin/dashboard" className='flex items-center py-2 bg-slate-100 rounded-md px-2 gap-3 text-xl sm:text-2xl mt-4 sm:mt-8 hover:bg-[#e2e8f0] transition-all duration-200 ease-in-out overflow-hidden text-nowrap'><MdSpaceDashboard className="min-w-8 "/>Dashboard</Link>
+        <Link href="/admin/users" className='flex items-center py-2 bg-slate-100 rounded-md px-2 gap-3 text-xl sm:text-2xl mt-1 hover:bg-[#e2e8f0] transition-all duration-200 ease-in-out overflow-hidden text-nowrap '><FaUserFriends className="min-w-8 "/>Users</Link>
+        <Link href="/admin/products" className='flex items-center py-2 bg-slate-100 rounded-md px-2 gap-3 text-xl sm:text-2xl mt-4 sm:mt-8 hover:bg-[#e2e8f0] transition-all duration-200 ease-in-out overflow-hidden text-nowrap '><FaShoppingCart className="min-w-8 "/>Products</Link>
+        <Link href="/admin/addproduct" className='flex items-center py-2 bg-slate-100 rounded-md px-2 gap-3 text-xl sm:text-2xl mt-1 hover:bg-[#e2e8f0] transition-all duration-200 ease-in-out overflow-hidden text-nowrap '><FaPlusSquare className="min-w-8 "/>Add Products</Link>
+        <Link href="/admin/orders" className='flex items-center py-2 bg-slate-100 rounded-md px-2 gap-3 text-xl sm:text-2xl mt-4 sm:mt-8 hover:bg-[#e2e8f0] transition-all duration-200 ease-in-out overflow-hidden text-nowrap '><FaNotesMedical className="min-w-8 "/>Orders</Link>
+        <Link href="/admin/cancelledorders" className='flex items-center py-2 bg-slate-100 rounded-md px-2 gap-3 text-xl sm:text-2xl mt-1 hover:bg-[#e2e8f0] transition-all duration-200 ease-in-out overflow-hidden text-nowrap '><FaTimes className="min-w-8 "/>Cancelled Orders</Link>
+        <Link href="/admin/payments" className='flex items-center py-2 bg-slate-100 rounded-md px-2 gap-3 text-xl sm:text-2xl mt-4 sm:mt-8 hover:bg-[#e2e8f0] transition-all duration-200 ease-in-out overflow-hidden text-nowrap '><FaCcAmazonPay className="min-w-8 "/>Payments</Link>
       </div>
   )
 }
