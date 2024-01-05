a24eb0988a259d7fdb4da8d0bbd9b6e76ab89b94
Abu Zaid <zaid70979@gmail.com>
2024-01-06 01:45:24 +0530

    frontend: manage user page

--- frontend/src/app/admin/users/page.tsx
+++ frontend/src/app/admin/users/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 import React, { useState } from 'react'
-import { FaTrash, FaEdit, FaStar } from "react-icons/fa";
-import Link from 'next/link';
+import { FaTrash} from "react-icons/fa";
 import AdminSiderbar from '@/app/components/AdminSiderbar';
 import { Roboto_Slab } from "next/font/google"
 
@@ -13,52 +12,48 @@ const robotoSlab = Roboto_Slab({
 
 const page = () => {
   const [showConfirm, setShowConfirm] = useState(false)
-  const rat = 5
   return (
     <div className='flex bg-slate-200'>
       <AdminSiderbar />
       <div className='pl-16 sm:pl-20 w-full min-h-[100vh] overflow-hidden overflow-y-scroll text-slate-700' >
-        <h1 className={`${robotoSlab.className} text-4xl sm:text-5xl text-center my-6 text-main-800`}>Products</h1>
+        <h1 className={`${robotoSlab.className} text-4xl sm:text-5xl text-center my-6 text-main-800`}>Users</h1>
             <div className='w-full flex flex-col items-start p-4'>
               <input className=" outline-none w-full py-1 px-4 text-xl border-2 border-main-800 rounded-md rounded-bl-none shadow-md max-w-[600px]" type="search"  placeholder='Search Products' />
               <label className='bg-main-800 text-white py-1 px-2 sm:px-4 rounded-b-md sm:text-xl shadow-md'>Search Type:- 
                 <select className='text-slate-700 sm:px-4 ml-2 outline-none rounded-md cursor-pointer'>
-                  <option value={"default"}>Default</option>
-                  <option value={"stock"}>Stock</option>
-                  <option value={"price"}>Price</option>
-                  <option value={"rating"}>Rating</option>
-                  <option value={"id"}>Product Id</option>
+                  <option value={"all"}>All</option>
+                  <option value={"user"}>User</option>
+                  <option value={"admin"}>Admin</option>
                 </select>
               </label>
             </div>
-            <div className='flex items-center justify-end w-full px-6'><span className='bg-main-800 text-white text-xl py-1 px-4 rounded-md shadow-md'>Total Products: 20</span></div>
+            <div className='flex items-center justify-end w-full px-6'><span className='bg-main-800 text-white text-xl py-1 px-4 rounded-md shadow-md'>Total Users: 20</span></div>
 
 
         <table className='mt-4'>
           <thead className='bg-main-800 border-main-800 border-2'>
             <tr>
-              <th>Thumbnail</th>
-              <th className='name'>Name</th>
-              <th>Stock</th>
-              <th>Price</th>
-              <th>Rating</th>
-              <th>Edit</th>
+              <th>Profile</th>
+              <th>User Id</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Type</th>
               <th>Delete</th>
             </tr>
           </thead>
 
           <tbody>
             <tr>
-              <td className='border border-main-800' aria-label='Thumbnail'><img style={{ height: "60px", width: "60px", margin: " 8px auto", borderRadius: '5px' }} src={'/img/5.jpg'} alt="Pic" /></td>
-              <td className='name border border-main-800' aria-label={"Name"}>
-                <Link href={`/details`}><p className='w-full max-h-28 overflow-hidden'>Testy Food Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eligendi nisi earum?</p></Link>
+              <td className='border border-main-800' aria-label='Profile'><img style={{ height: "60px", width: "60px", margin: " 8px auto", borderRadius: '5px' }} src={'/img/5.jpg'} alt="Pic" /></td>
+              <td className='border border-main-800' aria-label={"User Id"}>64fabc528eb710bd3d00f277</td>
+              <td className='border border-main-800' aria-label={"Name"}>Abu Zaid</td>
+              <td className='border border-main-800' aria-label={"Email"}>zaid70979@gmail.com</td>
+              <td className='border border-main-800' aria-label={"Type"}>
+                <select className='text-slate-700 sm:px-4 ml-2 outline-none rounded-md cursor-pointer border-2 border-main-800'>
+                  <option value={"user"}>User</option>
+                  <option value={"admin"}>Admin</option>
+                </select>
               </td>
-              <td className='border border-main-800' aria-label={"Stock"}>20</td>
-              <td className='border border-main-800' aria-label={"Price"}>&#8377; 2000</td>
-              <td className='border border-main-800' aria-label={"Rating"}>
-                <span className={`${(rat > 3) ? 'bg-green-700' : ""} ${(rat <= 3 && rat > 1) ? 'bg-yellow-500' : ""} ${(rat <= 1) ? 'bg-red-700' : ""} text-white flex m-auto items-center justify-evenly`} style={{ width: '50px', height: "30px", borderRadius: '8px' }}>{rat}<FaStar /></span>
-              </td>
-              <td className='border border-main-800' aria-label={"Edit"}><Link href={`/admin/updateproduct`}><FaEdit className='icon edit' /></Link></td>
               <td className='border border-main-800' aria-label={"Delete"}><FaTrash className='icon delete' onClick={() => { setShowConfirm(true) }} /></td>
             </tr>
             
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Mukta } from 'next/font/google'
 import './globals.css'
+import './CSS/table.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
